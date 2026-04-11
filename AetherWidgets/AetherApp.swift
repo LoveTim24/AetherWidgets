c619402ae3d71e6b0ee52dc8266c13097be11ca9
LoveTim24 <lovetimeven@gmail.com>
2026-04-11 23:08:46 +0300

    Add files via upload

---
+++ AetherWidgets/AetherApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct AetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
